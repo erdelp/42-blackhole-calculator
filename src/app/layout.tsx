fbd31e682680d2030c46d821fc51ad32a72bbc3f
Eric Delplancq <edelplan@f2r11s18.clusters.42paris.fr>
2025-07-22 20:31:58 +0200

    fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,14 +9,14 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "42 Blackhole Calculator",
-  description: "Calculate your blackhole date for 42",
+  description: "Calculate your blackhole date for 42 🕳️ ",
   keywords: "42 school, blackhole calculator, 42 programming, 42 milestones, 42 pace, 42 paris",
-  authors: [{ name: "edelplan" }],
+  authors: [{ name: "erdelp" }],
   openGraph: {
     title: "42 Blackhole Calculator",
-    description: "Calculate your blackhole date for 42",
+    description: "Calculate your blackhole date for 42 🕳️",
     type: "website",
-    images: ["/42-blackhole-calculator/logo/42_Logo.svg.png"],
+    images: ["data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='80' font-size='90'%3E🕳️%3C/text%3E%3C/svg%3E"],
   },
   icons: {
     icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>●</text></svg>",
