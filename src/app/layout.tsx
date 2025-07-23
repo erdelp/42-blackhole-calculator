1b97733b3007b1305add2ee4e4b0659104f17254
Eric Delplancq <edelplan@f2r11s16.clusters.42paris.fr>
2025-07-23 18:40:11 +0200

    improved freeze day phrasing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     images: ["data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='80' font-size='90'%3E🕳️%3C/text%3E%3C/svg%3E"],
   },
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>●</text></svg>",
+    icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='80' font-size='90'%3E🕳️%3C/text%3E%3C/svg%3E",
   },
 };
 
